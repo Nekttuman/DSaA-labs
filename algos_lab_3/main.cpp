1762405943b7aeeb655af398763d333e54f48983
Nekttuman <nekttuman@gmail.com>
2022-10-25 17:31:43 +0300

    work on decode algorithm

--- algos_lab_3/main.cpp
+++ algos_lab_3/main.cpp
@@ -5,6 +5,8 @@
 #include <memory>
 #include <vector>
 #include <algorithm>
+#include <math.h>
+#include <limits>
 
 
 const std::string pref = "../input_texts/";
@@ -37,7 +39,7 @@ class {
         in.close();
 
         for (auto i: source_alphabet)
-            std::cout<<i<<" ";
+            std::cout << i << " ";
     }
 
     void set_encr_table(short mode = 1) {
@@ -79,81 +81,106 @@ public:
 class {
     std::map<char, int> freq_table;
     std::map<char, int> standard_freq_table;
-    std::map<char,char> decode_table;
+    std::map<char, char> decode_table;
+    std::map<std::vector<char>, int> trigrams_freq;
+    // .first looks like {'t','h','e'} for "the" trigramm
+    // .second is frequency value
+
+    double decode_error = std::numeric_limits<double>::infinity();
 
 
-    void frequency_count(){
+    void frequency_count() {
         std::fstream in(ENCR_FILE_PATH, std::ios_base::in);
 
-        for (char ch = -1; !in.eof(); in>>std::noskipws>>ch){
-            if (ch != -1)
+        for (char ch = -1; !in.eof(); in >> std::noskipws >> ch) {
+            if (ch != -1 && ch!= '\n')
                 ++freq_table[ch];
         }
         in.close();
 
-        for(auto s: freq_table)
-            std::cout<<s.first<<" : "<<s.second<<'\n';
+        //for (auto s: freq_table)
+        //std::cout << s.first << " : " << s.second << '\n';
     }
 
-    void get_standard_freq_table(){
+    void get_standard_freq_table() {
         std::fstream in(FREQ_TABLE_PATH, std::ios_base::in);
 
         char ch;
         int freq;
-        while (!in.eof()){
-            in>> ch>>freq;
+        while (!in.eof()) {
+            in >> ch >> freq;
             standard_freq_table.insert(std::pair<char, int>(ch, freq));
         }
-        standard_freq_table.insert(std::pair<char, int> (' ', 200000));
-
+        standard_freq_table.insert(std::pair<char, int>(' ', 200000));
     }
 
-    void create_decode_table(){
+    void create_decode_table() {
         auto _freq_table = freq_table;
         auto _standard_freq_table = standard_freq_table;
 
         unsigned int s_freq_t_len = standard_freq_table.size();
-        for (int i = 0; i<s_freq_t_len; ++i){
-            auto s_mx = std::max_element(_standard_freq_table.begin(), _standard_freq_table.end(),[] (const std::pair<char, int> & p1, const std::pair<char, int> & p2) {
+        for (int i = 0; i < s_freq_t_len; ++i) {
+            auto cmpr = [](const std::pair<char, int> &p1, const std::pair<char, int> &p2) {
                 return p1.second < p2.second;
-            });
-            auto mx = std::max_element(_freq_table.begin(), _freq_table.end(), [] (const std::pair<char, int> & p1, const std::pair<char, int> & p2) {
-                return p1.second < p2.second;
-            });
-            decode_table.insert(std::pair<char,char>(mx->first, s_mx->first));
+            };
+            auto s_mx = std::max_element(_standard_freq_table.begin(), _standard_freq_table.end(), cmpr);
+            auto mx = std::max_element(_freq_table.begin(), _freq_table.end(), cmpr);
+            decode_table.insert(std::pair<char, char>(mx->first, s_mx->first));
 
-
-            //std::cout<<mx->first<<" : "<< s_mx->first<<"\n";
 
             _freq_table.erase(mx);
             _standard_freq_table.erase(s_mx);
         }
-        decode_table.insert(std::pair<char,char>('\n','\n'));
+        decode_table.insert(std::pair<char, char>('\n', '\n'));
     }
 
-    void try_decode(){
+    void decode() {
         std::fstream in(ENCR_FILE_PATH, std::ios_base::in);
         std::fstream out(DECR_FILE_PATH, std::ios_base::out);
 
-
-        for (char ch = ' '; !in.eof(); in>>std::noskipws>>ch){
-            out<<decode_table[ch];
+        for (char ch = ' '; !in.eof(); in >> std::noskipws >> ch) {
+            if (ch != ' ') {
+                out << decode_table[ch];
+                std::cout << decode_table[ch];
+            }
         }
 
         in.close();
         out.close();
     }
+
+
+
+    void update_decode_table() {
+
+    }
+
+    double get_error() {
+        return 0;
+    }
+
 public:
-    void operator ()(){
-            frequency_count();
-            get_standard_freq_table();
-            create_decode_table();
-            try_decode();
+    void operator()() {
+        frequency_count();
+        get_standard_freq_table();
+        create_decode_table();
+
+        double new_decode_error, err_delta;
+        do {
+            decode();
+            new_decode_error = get_error();
+            err_delta = std::abs(new_decode_error - decode_error);
+            if (new_decode_error < decode_error) {
+                decode_error = new_decode_error;
+            }
+            update_decode_table();
+        } while (err_delta >= 0.001);  // set noramal value
     }
 } decryption;
 
+
 int main() {
-    make_encryption();
+    //make_encryption();
     decryption();
     return 0;
 }
