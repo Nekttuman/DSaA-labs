4d6a47c0673372eacc1cde96b736a63c17a63c18
TwoMunп <nekttuman@gmail.com>
2022-12-06 15:20:39 +0300

    Coursework: service files

--- kursach/main.cpp
+++ kursach/main.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
+#include "src/Algorithm.h"
+
+#include "test/AlgorithmTests.h"
 
 int main() {
-    
+//    Algorithm alg;
+//    alg.parseSvg("../test.svg");
+//    alg.logSvg();
+//    alg.setGcodeFilePath("../test.gcode");
+//
+//    alg.mergeIntersectFigures();
+//    alg.logSvg();
+//
+//    alg.sliceFigures();
+//    alg.generateGcode();
 
+    AlgorithmTests algTest;
+    algTest.startTests();
     return 0;
 }
