dea17ece53edd174d0e045ca8f61c5acdf89d4c3
TwoMunп <nekttuman@gmail.com>
2023-01-28 13:12:37 +0300

    Coursework: add scaling factor for gcode generator class

--- coursework/src/Gcode.cpp
+++ coursework/src/Gcode.cpp
@@ -5,4 +5,17 @@
 #include "Gcode.h"
 
 
+void Gcode::setPrinterSettings(double maxXPos, double maxYPos, double offsetX, double offsetY){
+    if (maxXPos <= offsetX || maxYPos <= offsetY)
+        throw;
+    maxYPos_m = maxYPos;
+    maxXPos_m = maxXPos;
+    brushOffsetX_m = offsetX;
+    brushOffsetY_m = offsetY;
+}
+
+void Gcode::setSvgResolution(double svgResolutionX, double svgResolutionY) {
+    svgResolutionX_m = svgResolutionX;
+    svgResolutionY_m = svgResolutionY;
+}
 
--- coursework/src/Gcode.h
+++ coursework/src/Gcode.h
@@ -15,25 +15,33 @@ class Gcode {
 
     std::fstream file;
 
-    int brushOffsetX = 70, brushOffsetY = 60;
-    double maxYPos = 235, maxXPos = 235;  // 235-brushOffset
+    double brushOffsetX_m = 70, brushOffsetY_m = 60;
+    double maxYPos_m = 235, maxXPos_m = 235;  // 235-brushOffset
+    double svgResolutionX_m = 800, svgResolutionY_m = 600;
+    double xKoeff, yKoeff;
 public:
     Gcode() = default;
 
-    void insertInitGcode() {
-        file << "G28 ; calibration\n";
-        file<<"G1 X" + std::to_string(brushOffsetX + 0.3) <<"Z3 F5000.0 \n";
-        file << "G1 X" + std::to_string(brushOffsetX + 0.3) + " Y" + std::to_string(brushOffsetY + 0.3) +
-                " Z0 F5000.0 ; Move to start position\n";
+    void setPrinterSettings(double maxXPos, double maxYPos, double offsetX, double offsetY);
 
+    void setSvgResolution(double svgResolutionX, double svgResolutionY);
+
+    void insertInitGcode() {
+        xKoeff = (maxXPos_m - brushOffsetX_m) / svgResolutionX_m;
+        yKoeff = (maxYPos_m - brushOffsetY_m) / svgResolutionY_m;
+        file << "G28 ; calibration\n";
+        file << "G1 X" + std::to_string(brushOffsetX_m + 0.3) << "Z3 F5000.0 \n";
+        file << "G1 X" + std::to_string(brushOffsetX_m + 0.3) + " Y" + std::to_string(brushOffsetY_m + 0.3) +
+                " Z0 F5000.0 ; Move to start position\n";
     }
 
     void moveToStart() {
-        file << "\n\nG1 X" + std::to_string(brushOffsetX) + " Y" + std::to_string(brushOffsetY) +
+        file << "\n\nG1 X" + std::to_string(brushOffsetX_m) + " Y" + std::to_string(brushOffsetY_m) +
                 " Z0 F5000.0\n";
     }
+
     void moveToEnd() {
-        file << "\n\nG1 X" + std::to_string(brushOffsetX) + " Y" + std::to_string(brushOffsetY) +
+        file << "\n\nG1 X" + std::to_string(brushOffsetX_m) + " Y" + std::to_string(brushOffsetY_m) +
                 " F5000.0\nG1 X0 Y0\n\n";
     }
 
@@ -48,34 +56,34 @@ public:
     void moveTo(fig::Point p) {
         raiseBrush();
         file << "; move to point (" << p.x << ":" << p.y << ")\n"
-             << "G1 X" << maxXPos - (p.x* 165/800) << " Y" << maxYPos - p.y*165/600 << " F3000\n";
+             << "G1 X" << maxXPos_m - p.x * xKoeff << " Y" << maxYPos_m - p.y * yKoeff << " F3000\n";
         lowerBrush();
 
     }
 
     void drawTo(fig::Point p) {
-        file << "G1 X" <<maxXPos - (p.x* 165/800) << " Y" << maxYPos - p.y*165/600 << " F3000; draw\n";
+        file << "G1 X" << maxXPos_m - (p.x * 165 / 800) << " Y" << maxYPos_m - p.y * 165 / 600 << " F3000; draw\n";
     }
 
     void sliceFig(const fig::slicedFig &fig) {
         switch (fig.ft) {
             case fig::CIRCLE: {
-                file<<"\n;CIRCLE\n";
+                file << "\n;CIRCLE\n";
                 moveTo(fig.points.front());
-                for (auto i = ++fig.points.begin();i != fig.points.end();++i)
+                for (auto i = ++fig.points.begin(); i != fig.points.end(); ++i)
                     drawTo(*i);
                 break;
             }
             case fig::SEGMENT: {
-                file<<"\n;SEGMENT\n";
+                file << "\n;SEGMENT\n";
                 moveTo(fig.startP);
                 drawTo(fig.endP);
                 break;
             }
             case fig::RECT: {
-                file<<"\n;RECT\n";
+                file << "\n;RECT\n";
                 moveTo(fig.points.front());
-                for (auto i = ++fig.points.begin();i != fig.points.end();++i)
+                for (auto i = ++fig.points.begin(); i != fig.points.end(); ++i)
                     drawTo(*i);
                 break;
             }
